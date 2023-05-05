fb20cd503ac3533eabb3e19dfcf0fbcb65c6ad97
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2023-05-05 09:01:33 +0500

    The "global" object in Node is defined

---
+++ Node/01_global.js
@@ -0,0 +1,18 @@
+/* ==== Node global object ====*/
+// 'global' is the global object in node context like 'window' objects in the browser
+// console.log(global);
+
+// runs once after the specified time
+global.setTimeout(() => { // we can omit the global object b/c its available in the node context
+    console.log('In the Timeout');
+    clearInterval(interval); // clears the interval
+}, 3000);
+
+// runs forever after the specified time
+const interval = global.setInterval(() => {
+    console.log('In the Interval');
+}, 1000);
+
+// console.log(__dirname);
+// console.log('----');
+// console.log(__filename);
