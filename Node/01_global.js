a8cfd4a45e996568172b654f11d656817c565d0b
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2023-05-16 08:11:10 +0500

    file updated for testing purposes

--- Node/01_global.js
+++ Node/01_global.js
@@ -3,16 +3,16 @@
 // console.log(global);
 
 // runs once after the specified time
-global.setTimeout(() => { // we can omit the global object b/c its available in the node context
-    console.log('In the Timeout');
-    clearInterval(interval); // clears the interval
-}, 3000);
+// global.setTimeout(() => { // we can omit the global object b/c its available in the node context
+//     console.log('In the Timeout');
+//     clearInterval(interval); // clears the interval
+// }, 3000);
 
 // runs forever after the specified time
-const interval = global.setInterval(() => {
-    console.log('In the Interval');
-}, 1000);
+// const interval = global.setInterval(() => {
+//     console.log('In the Interval');
+// }, 1000);
 
-// console.log(__dirname);
-// console.log('----');
-// console.log(__filename);
+console.log(__dirname);
+console.log('----');
+console.log(__filename);
