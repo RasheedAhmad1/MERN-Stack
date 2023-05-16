d0aec9dd7724ffd3127ea300ff483aff4a3fc27c
Rasheed_Ahmad <rasheedahmad22333@gmail.com>
2023-05-16 08:11:47 +0500

    Data file added for testing purposes

---
+++ Node/modules/data.js
@@ -0,0 +1,21 @@
+// A file holding data for testing purposes
+const alphabets = ['b','c','d','f','...'];
+const consonents = ['a','e','i','o','u'];
+
+// exporting a string
+// module.exports = 'hello';
+
+// exporting the array
+// module.exports = alphabets;
+
+// exporting multiple different things (object exporting)
+// module.exports = {
+//     alphabets: alphabets,
+//     consonents: consonents
+// }
+
+// OR
+module.exports = {
+    alphabets,
+    consonents
+};
